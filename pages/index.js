fa30d4d44b5d2f9d54a2b9d5a2be96ffe9a113a6
Mhmd-Kamal <Mhmd-Kamal>
2022-08-18 01:05:13 +0200

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      Hello
+    </div>
+  )
+}
+
+export default Home
