d4a27b7506d483d3262269574cd9c5f9a2c7cab9
Mhmd-Kamal <Mhmd-Kamal>
2022-08-24 13:49:37 +0200

    add head tags

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 import NavBar from '../components/NavBar'
 import Intro from '../components/Intro'
@@ -7,6 +6,16 @@ import Testmonials from '../components/TestmonialCarousel/Testmonials'
 import Footer from '../components/Footer'
 
 const Home = () => {
+  ;<Head>
+    <title>Manage</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view."
+    />
+    <meta name="keywords" content="manage software teams" />
+  </Head>
   return (
     <div className="flex min-h-screen flex-col text-Dark-Blue">
       <NavBar />
