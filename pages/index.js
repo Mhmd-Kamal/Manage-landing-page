79d0008d62d3c7510d5fa747e07781c7516172ce
Mhmd-Kamal <Mhmd-Kamal>
2022-08-24 13:58:45 +0200

    add favicon

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,20 @@ import Testmonials from '../components/TestmonialCarousel/Testmonials'
 import Footer from '../components/Footer'
 
 const Home = () => {
-  ;<Head>
-    <title>Manage</title>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta
-      name="description"
-      content="Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view."
-    />
-    <meta name="keywords" content="manage software teams" />
-  </Head>
   return (
     <div className="flex min-h-screen flex-col text-Dark-Blue">
+      <Head>
+        <title>Manage</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view."
+        />
+        <meta name="keywords" content="manage software teams" />
+        <link rel="shortcut icon" href="/images/logo.svg" type="image/x-icon" />
+      </Head>
+
       <NavBar />
       <Intro />
       <Testmonials />
